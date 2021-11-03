8687c99a61ac907353acf0ee3ce2e38d6ccc0b77
Jose Guardo <joseguardomcnish@yahoo.es>
2021-11-02 22:44:08 -0500

    agregar despliegue de dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Home: NextPage = () => {
   return (
     <div className='bg-green-500'>
-     <p> Pagina Index</p>
+     <p> Pagina Index - pueba dev</p>
        <Link href='/admin/usuarios'>
          <p className='cursor-pointer'> Ir aa admin de usuarios</p>
        </Link>
